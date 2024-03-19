42eb174b20f7329ef78ffbca768158b0a05f6106
Christian-Lepidi <christianlepidi@gmail.com>
2024-03-19 18:15:16 +0100

    feat: add. Models connected with Controller

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -4,12 +4,15 @@ namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Movie;
 
 class PageController extends Controller
 {
     // index e la view con le card
     public function index()
     {
+        $movies = Movie::all();
+        dd($movies);
         return view('home');
     }
 }
